c25debdb614d7fb3b25e37dd4050cf205614887f
Azeem-py <74514285+Azeem-py@users.noreply.github.com>
2024-02-15 22:52:51 +0100

    mobile responsiveness touch

--- src/component/FormHeader.jsx
+++ src/component/FormHeader.jsx
@@ -1,8 +1,8 @@
 const FormHeader = () => {
   return (
     <div className='text-[#1D2329] font-bold text-center'>
-      <h2 className='text-5xl mb-3'>Sign in</h2>
-      <h3 className='text-[1.7rem] leading-7 text-wrap'>
+      <h2 className='text-4xl md:text-5xl mb-3'>Sign in</h2>
+      <h3 className='text-[1.5rem] md:text-[1.7rem] leading-7 text-wrap'>
         to access AT&T Mail and Currently.com
       </h3>
     </div>
--- src/page/Signin.jsx
+++ src/page/Signin.jsx
@@ -47,8 +47,8 @@ const Signin = () => {
 
   return (
     <div className='md:h-screen md:w-screen flex items-center justify-between flex-col'>
-      <main className='md:border rounded-xl mt-20 flex flex-col items-center md:w-[446px] p-[48px]'>
-        <img src={AttSVG} width={120} className='mb-10' />
+      <main className='md:border rounded-xl mt-8 md:mt-20 flex flex-col items-center md:w-[446px] p-[20px] md:p-[48px]'>
+        <img src={AttSVG} width={140} className='mb-10' />
         <section>
           {!showPassword ? (
             <FormHeader />
@@ -70,13 +70,13 @@ const Signin = () => {
             <p className='text-red-800 font-semibold mt-2'>{error}</p>
 
             <button
-              className='text-[#F2FAFD] font-semibold bg-blue-800 hover:bg-blue-700 w-full py-3 rounded-full mt-8 outline-blue-700'
+              className='text-[#F2FAFD] font-semibold bg-blue-800 hover:bg-blue-700 w-full py-3 rounded-full mt-5 md:mt-8 outline-blue-700'
               onClick={handleContinue}
             >
               {!showPassword ? 'Continue' : 'Sign in'}
             </button>
 
-            <footer className='mt-7 text-[#0057B8] font-bold flex flex-col gap-5'>
+            <footer className='mt-7 text-[#0057B8] font-bold flex flex-col gap-3 md:gap-5'>
               <p>Forgot user ID? </p>
               <p>Don't have a user ID? Create one now</p>
             </footer>
